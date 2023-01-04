9069b9a7be4e93a9d26edbc7c54a35bf7b201147
atlass-dev <73686943+atlass-dev@users.noreply.github.com>
2023-01-04 19:42:44 +0500

    Profile page added

--- Secrets Sharing/Controllers/ProfileController.cs
+++ Secrets Sharing/Controllers/ProfileController.cs
@@ -1,13 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Secrets_Sharing.DAL.Interfaces;
+using Secrets_Sharing.Domain.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Secrets_Sharing.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index()
-        {
+        private readonly IUserRepository _userRepository;
 
-            return View();
+        public ProfileController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var email = User.Identity.Name;
+            var users = await _userRepository.GetAll();
+            var user = users.FirstOrDefault(u => u.Email == email);
+
+            return View(user);
         }
     }
 }
